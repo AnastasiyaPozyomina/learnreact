5e56e71eab7f20af41bf54197feb029286828216
AnastasiyaPozyomina <pozyomina@gmail.com>
2020-10-14 01:31:21 +0700

    components

---
+++ src/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+
+class Footer extends React.Component {
+    render() {
+        return ( 
+            <footer class="section footer-classic context-dark bg-image" style = {{background:"#2d3246", color: "#ffff"}}> Copyright </footer>
+        )
+    }
+}
+
+export default Footer;
---
+++ src/main/Main.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './Main.css';
+
+
+class Main extends React.Component {
+    render() {
+        return ( 
+            <div className="container">
+            <main>
+            <article>
+            <header>
+            <h1> Article 1 </h1> 
+            <h2 > Article 2 </h2> 
+            </header> 
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.Magni officia tempora voluptatibus nisi quia, corporis cumque vel veritatis.Itaque exercitationem, blanditiis nemo similique repellendus voluptas minima ? Placeat tenetur sed aperiam. </p> 
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.Magni officia tempora voluptatibus nisi quia, corporis cumque vel veritatis.Itaque exercitationem, blanditiis nemo similique repellendus voluptas minima ? Placeat tenetur sed aperiam. </p> 
+            </article> 
+            </main>
+            </div>
+        )
+    }
+}
+
+export default Main;
