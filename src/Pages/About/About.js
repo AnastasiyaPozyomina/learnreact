ae664acbf8aff2b21ec465f2a629168fa836ed49
AnastasiyaPozyomina <pozyomina@gmail.com>
2020-10-15 03:52:23 +0700

    gallery

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -7,6 +7,7 @@ export default class About extends React.Component {
     render() {
         return ( 
             <div><h1>About</h1></div>
+            
         )
     }
 }
--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ImageGridList from '../../components/imageGridList/ImageGridList';
 import './Blog.css';
 
 
@@ -7,7 +8,10 @@ import './Blog.css';
   export default class Blog extends React.Component {
     render() {
         return ( 
-            <div>Блог</div>
+            <div>
+            <h1>Блог</h1>
+            <ImageGridList />
+            </div>
         );
     }
     }
