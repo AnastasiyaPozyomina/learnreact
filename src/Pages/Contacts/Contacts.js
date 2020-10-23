2e95686db674ece6835df4d6e086bd14e32e1663
AnastasiyaPozyomina <pozyomina@gmail.com>
2020-10-23 17:12:17 +0700

    компонент и клиенты добавлены на страницу Contact Us

--- src/Pages/Contacts/Contacts.js
+++ src/Pages/Contacts/Contacts.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import MainContent from '../../components/main_content/main_content';
-
+import Clients from '../../components/clients/clients';
+import Footer from '../../components/footer/Footer';
 
 
 export default class Contact extends React.Component {
   render () {
     return (
-    <MainContent h2="LET’S GET IN TOUCH" h4="CONTACT US"/>  
+    <div>
+    <MainContent h2="LET’S GET IN TOUCH" h4="CONTACT US"/>
+    <Clients />
+    <Footer />
+    </div>
     );
   }
 }
