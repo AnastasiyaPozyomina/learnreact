c59f37797f83645141ecfb65e19a4cf79d4550e2
AlexBukhtatyy <buhtatyalexander90@gmail.com>
2020-10-31 12:28:48 +0300

    refactor(app/formsend): удалил ненужный тег fieldset

--- src/components/FormSend/FormSend.js
+++ src/components/FormSend/FormSend.js
@@ -1,33 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 export default class FormSend extends Component {
-  constructor () {
-    super ();
+  constructor() {
+    super();
     this.state = {
-      firstName: '',
-      email: '',
-      subject: '',
-      message: '',
-      
+      firstName: "",
+      email: "",
+      subject: "",
+      message: "",
     };
-    this.handleChange = this.handleChange.bind (this);
-    this.handleSubmit = this.handleSubmit.bind (this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange (event) {
-    const {name, value} = event.target;
-    this.setState ({
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
       [name]: value,
     });
-
   }
 
+  handleSubmit = () => {};
 
-  handleSubmit = () => {
-
-  }
-
-  render () {
+  render() {
     return (
       <div>
         <div class="send-message">
@@ -48,80 +43,67 @@ export default class FormSend extends Component {
                   >
                     <div class="row">
                       <div class="col-lg-12 col-md-12 col-sm-12">
-                        <fieldset>
-                          <input
-                            type="text"
-                            name="firstName"
-                            class="form-control"
-                            id="name"
-                            placeholder="Full Name"
-                            required
-                            value={this.state.firstName}
-                            onChange={this.handleChange}
-                          />
-                        </fieldset>
+                        <input
+                          type="text"
+                          name="firstName"
+                          class="form-control"
+                          id="name"
+                          placeholder="Full Name"
+                          required
+                          value={this.state.firstName}
+                          onChange={this.handleChange}
+                        />
                       </div>
                       <div class="col-lg-12 col-md-12 col-sm-12">
-                        <fieldset>
-                          <input
-                            type="text"
-                            name="email"
-                            class="form-control"
-                            id="email"
-                            placeholder="E-Mail Address"
-                            required=""
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                          />
-                        </fieldset>
+                        <input
+                          type="text"
+                          name="email"
+                          class="form-control"
+                          id="email"
+                          placeholder="E-Mail Address"
+                          required=""
+                          value={this.state.email}
+                          onChange={this.handleChange}
+                        />
                       </div>
                       <div class="col-lg-12 col-md-12 col-sm-12">
-                        <fieldset>
-                          <input
-                            name="subject"
-                            type="text"
-                            class="form-control"
-                            id="subject"
-                            placeholder="Subject"
-                            required=""
-                            value={this.state.subject}
-                            onChange={this.handleChange}
-                          />
-                        </fieldset>
+                        <input
+                          name="subject"
+                          type="text"
+                          class="form-control"
+                          id="subject"
+                          placeholder="Subject"
+                          required=""
+                          value={this.state.subject}
+                          onChange={this.handleChange}
+                        />
                       </div>
                       <div class="col-lg-12">
-                        <fieldset>
-                          <textarea
-                            name="message"
-                            rows="6"
-                            class="form-control"
-                            id="message"
-                            placeholder="Your Message"
-                            required
-                            value={this.state.message}
-                            onChange={this.handleChange}
-                            
-                          />
-                        </fieldset>
+                        <textarea
+                          name="message"
+                          rows="6"
+                          class="form-control"
+                          id="message"
+                          placeholder="Your Message"
+                          required
+                          value={this.state.message}
+                          onChange={this.handleChange}
+                        />
                       </div>
                       <div class="col-lg-12">
-                        <fieldset>
-                          <input
-                            type="submit"
-                            value="Send Message"
-                            id="form-submit"
-                            class="filled-button" 
-                          />
-                        </fieldset>
+                        <input
+                          type="submit"
+                          value="Send Message"
+                          id="form-submit"
+                          class="filled-button"
+                        />
                       </div>
                     </div>
                   </form>
                 </div>
               </div>
-              </div>
-              <div class="col-md-4">
-              
             </div>
+            <div class="col-md-4"></div>
           </div>
         </div>
       </div>
