5fe38f9930ce058a4732ff7c56080f4e4aa981e3
AnastasiyaPozyomina <pozyomina@gmail.com>
2021-01-02 17:48:20 +0700

    Updates

---
+++ precache-manifest.025605312d421c6f0799dcec00cb5d6c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d235921798f056b91f58929e47146cf",
+    "url": "/learnreact/index.html"
+  },
+  {
+    "revision": "7e59e0eef54fd36e871b",
+    "url": "/learnreact/static/css/2.1bab9872.chunk.css"
+  },
+  {
+    "revision": "16fb6a4f6630c907f1fd",
+    "url": "/learnreact/static/css/main.074770ff.chunk.css"
+  },
+  {
+    "revision": "7e59e0eef54fd36e871b",
+    "url": "/learnreact/static/js/2.7860035b.chunk.js"
+  },
+  {
+    "revision": "1c42f926d85df35b12ef99c84c58de1e",
+    "url": "/learnreact/static/js/2.7860035b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16fb6a4f6630c907f1fd",
+    "url": "/learnreact/static/js/main.4e933cd2.chunk.js"
+  },
+  {
+    "revision": "bb48dbdf78da26e54973",
+    "url": "/learnreact/static/js/runtime-main.26c69b6e.js"
+  },
+  {
+    "revision": "2980083682e94d33a66eef2e7d612519",
+    "url": "/learnreact/static/media/fontawesome-webfont.29800836.svg"
+  },
+  {
+    "revision": "706450d7bba6374ca02fe167d86685cb",
+    "url": "/learnreact/static/media/fontawesome-webfont.706450d7.ttf"
+  },
+  {
+    "revision": "97493d3f11c0a3bd5cbd959f5d19b699",
+    "url": "/learnreact/static/media/fontawesome-webfont.97493d3f.woff2"
+  },
+  {
+    "revision": "d9ee23d59d0e0e727b51368b458a0bff",
+    "url": "/learnreact/static/media/fontawesome-webfont.d9ee23d5.woff"
+  },
+  {
+    "revision": "f7c2b4b747b1a225eb8dee034134a1b0",
+    "url": "/learnreact/static/media/fontawesome-webfont.f7c2b4b7.eot"
+  }
+]);
