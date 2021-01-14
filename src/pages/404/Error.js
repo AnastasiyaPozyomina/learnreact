81fdd9c9750ee0d1d83d794f910027d370c8048a
AnastasiyaPozyomina <pozyomina@gmail.com>
2021-01-14 16:28:43 +0700

    исправление ссылки в ERROR

--- src/pages/404/Error.js
+++ src/pages/404/Error.js
@@ -5,13 +5,13 @@ import './Error.css';
 function Error () {
   return (
     <div className="NotFind">
-      <NavLink className="link-error" to="/">Go Back</NavLink>
+      <NavLink className="link-error" to="/home">Go Back</NavLink>
       <div class="error-img">
         <img src="/learnreact/images/404-opt.png" alt="" />
       </div>
 
     </div>
   );
-};
+}
 
 export default Error;
